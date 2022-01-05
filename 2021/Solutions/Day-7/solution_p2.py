1e87ea9ff2530f65f39350b863f24dceafa145f1
Mikko Leppänen <mikko.leppanen@kone.com>
2022-01-05 15:45:14 +0200

    Added solution for day 7 parts 1 and 2.

---
+++ 2021/Solutions/Day-7/solution_p2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from pprint import PrettyPrinter
+from typing import List
+
+import numpy as np
+
+custom_printer = PrettyPrinter(
+    indent=4,
+    width=100,
+    depth=2,
+    compact=True,
+    sort_dicts=False,
+    underscore_numbers=True,
+)
+
+INPUT_FILE = "input.txt"
+
+
+def read_horizontal_positions() -> List[int]:
+    positions: List[int] = []
+    with open(INPUT_FILE, "r", encoding="utf-8") as f_handle:
+        for line in f_handle:
+            line = line.rstrip()
+            if line:
+                positions = [int(num) for num in line.split(",")]
+    return positions
+
+
+def find_position_with_least_fuel(positions: List[int]) -> int:
+    min_fuel = float("inf")
+    for position in sorted(range(min(positions), max(positions) + 1)):
+        fuel_amount = 0
+        for other_pos in [pos for pos in positions if pos != position]:
+            fuel_amount += np.cumsum([range(1, abs(other_pos - position) + 1)])[-1]
+        min_fuel = min(min_fuel, fuel_amount)
+    return min_fuel
+
+
+def solve() -> int:
+    positions = read_horizontal_positions()
+    return find_position_with_least_fuel(positions)
+
+
+def main():
+    fuel_consumption = solve()
+    print(f"Result: {fuel_consumption}")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
